46808da2bfab9f047e3f40891ecf18bff2c4c959
Ryan Brady <ryanbrady369@gmail.com>
2019-01-15 23:09:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
     DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT ?", num).map {|row| self.new_from_db(row)}
   end
 
-  def first_student_in_grade_10
+  def self.first_student_in_grade_10
     DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT 1").map {|row| self.new_from_db(row)}
   end
 
