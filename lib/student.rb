7139fa4eec73a4e90093ff90104e10b55b980a46
Ryan Brady <ryanbrady369@gmail.com>
2019-01-15 22:29:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,11 +18,11 @@ class Student
   end
 
   def self.all_students_in_grade_9
-    DB[:conn].execute("SELECT name FROM students WHERE grade = ?", 9).map {|row| self.new_from_db(row)}
+    DB[:conn].execute("SELECT name FROM students WHERE grade = ?", 9).map {|row| self.new_from_db(row)}.first
   end
 
   def self.students_below_12th_grade
-    DB[:conn].execute("SELECT name FROM students WHERE grade < ?", 12).map {|row| self.new_from_db(row)}
+    DB[:conn].execute("SELECT name FROM students WHERE grade < ?", 12).map {|row| self.new_from_db(row)}.first
   end
 
 
